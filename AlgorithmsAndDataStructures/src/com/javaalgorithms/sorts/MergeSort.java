650976e55ccebf7727215444e132fb5372a464c1
nelson-yeh-fy <nelson.yeh.fy@gmail.com>
2021-12-23 12:16:40 -0500

    - Add MergeSort - Add MergeSort Tests - Fix Merge Sort issue

--- AlgorithmsAndDataStructures/src/com/javaalgorithms/sorts/MergeSort.java
+++ AlgorithmsAndDataStructures/src/com/javaalgorithms/sorts/MergeSort.java
@@ -1,8 +1,5 @@
 package com.javaalgorithms.sorts;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author nelson-yeh-fy (https://https://github.com/nelson-yeh-fy)
  * @version 1.0
@@ -22,6 +19,13 @@ public class MergeSort implements SortingAlgorithm{
         return array;
     }
 
+    /**
+     * Recursively doing MergeSort by separating one big chunk of the array to two parts
+     * @param array the original array.
+     * @param i range starts from i
+     * @param j range ends at j
+     * @param <T> the sorted array
+     */
     private <T extends Comparable<T>> void sort(T[] array, int i, int j){
         if(i < j){
             int mid = i + (j - i) / 2;
@@ -31,16 +35,35 @@ public class MergeSort implements SortingAlgorithm{
         }
     }
 
+    /**
+     * The private function to merge two sorted arrays.
+     *
+     * @param array the array that we want to merge its two internally sorted ranges.
+     * @param start the first sorted range begins at "start", ends at "mid"
+     * @param mid the index we separate two part in the array
+     * @param end the second sorted range begins at "mid" + 1, ends at "end"
+     * @param <T> the same array but it's sorted when the function ends
+     */
     private <T extends Comparable<T>> void merge(T[] array, int start, int mid, int end) {
-        int i = mid, j = end;
-        while(j > start) {
-            if (array[i].compareTo(array[j]) <= 0) {
-                j--;
+        int i = mid, j = end; // Use two pointers to check values from two sorted range.
+        int len = end - start + 1;
+        int m = len - 1;
+        @SuppressWarnings("unchecked cast")
+        T[] tmp = (T[]) new Comparable[len];
+
+        while(i >= start && j > mid) {
+            if (array[i].compareTo(array[j]) < 0) {
+                tmp[m--] = array[j--];
             } else {
-                T temp = array[j];
-                array[j--] = array[i--];
-                array[i] = temp;
+                tmp[m--] = array[i--];
             }
         }
+        while(i >= start){
+            tmp[m--] = array[i--];
+        }
+        while(j > mid) {
+            tmp[m--] = array[j--];
+        }
+        System.arraycopy(tmp, 0, array, start, len);
     }
 }
--- AlgorithmsAndDataStructures/test/com/javaalgorithms/sorts/MergeSortTest.java
+++ AlgorithmsAndDataStructures/test/com/javaalgorithms/sorts/MergeSortTest.java
@@ -15,6 +15,13 @@ class MergeSortTest {
         }
         /* output: [1, 4, 6, 9, 12, 23, 54, 78, 231] */
 
+        Integer[] integers1 = {-5, 0, 78, -49, 0, 78, 99, -90};
+        mergeSort.sort(integers1);
+
+        for (int i = 0; i < integers1.length - 1; ++i) {
+            assert integers1[i] <= integers1[i + 1];
+        }
+        /* output: [-90, -49, -5, 0, 0, 78, 78, 99] */
 
         String[] strings = {"c", "a", "e", "b", "d"};
         mergeSort.sort(strings);
